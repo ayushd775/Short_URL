5d5000c3cd19347dd6bd4b76f5cd4104f863dc0f
ayushd775 <ayushdeshmukh775@gmail.com>
2024-09-24 15:42:15 +0530

    Refractored and setup

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express= require("express");
 
+const urlRoute= require('./routes/url');
+const {connectToMongo}= require('./connect');
+
 const app= express();
 const PORT=8001;
+
+connectToMongo("mongodb://localhost:27017/short-url")
+.then(()=> console.log("MongoDB connected"))
+
+app.use(express.json());
+
+app.use("/url",urlRoute);
 
 app.listen(PORT,()=> console.log(`Server started at ${PORT}`))
