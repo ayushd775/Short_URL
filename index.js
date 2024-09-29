94049ebf71fca64cfa0e3aae839acd05c5b76a3b
ayushd775 <ayushdeshmukh775@gmail.com>
2024-09-29 18:17:52 +0530

    signup functionality aaded

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express= require("express");
 const path= require("path");
+const {connectToMongo}= require('./connect');
+
 const urlRoute= require('./routes/url');
 const staticRoute=require('./routes/staticRouters');
-const {connectToMongo}= require('./connect');
+const userRoute=require('./routes/user');
 
 const app= express();
 const PORT=8001;
@@ -17,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use("/url",urlRoute);
-app.use("/",staticRoute)
+app.use("/",staticRoute);
+app.use("/user",userRoute);
 
 app.listen(PORT,()=> console.log(`Server started at ${PORT}`))
